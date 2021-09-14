c1b085b637362e56192115005d5ecdc5d0e90ce0
Crowdbotics <team@crowdbotics.com>
2021-09-14 01:39:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "damp_union_30529Identifier",
+  url: "https://damp_union_30529Identifier.botics.co",
+  api: "https://damp_union_30529Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
